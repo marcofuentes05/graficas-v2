57b0406af41b57ff57abdebc14c9e4b6ddb4b4d4
Marco <marcofuentes1607@gmail.com>
2020-10-15 14:35:13 -0600

    Proyecto DONE

--- Proyecto.py
+++ Proyecto.py
@@ -9,66 +9,56 @@ from AABB import *
 from sphere import *
 import random
 
-# Dedinicion de materiales
+# Definicion de materiales
 WHITE = Material(diffuse = color( 1,1,1 ) , spec= 16 )
 brick = Material(diffuse = color(0.8, 0.25, 0.25 ), spec = 16)
 stone = Material(diffuse = color(0.4, 0.4, 0.4 ), spec = 32)
 orange = Material(diffuse = color(1, 0.6, 0.0 ), spec = 32)
-
 mirror = Material(spec = 64, matType = REFLECTIVE)
 glass = Material(spec = 64, ior = 1.5, matType= TRANSPARENT)
-
 boxMat = Material(texture = Texture('./Assets/box.bmp'))
 earthMat = Material(texture = Texture('./Assets/earthDay.bmp') )
 sun = Material(texture=Texture('./Assets/sun.bmp'))
+venus = Material(texture=Texture('./Assets/venus.bmp'))
 
 # Variables de iniciacion de render
-width = 256
-height = 256
+width = 1920
+height = 1080
 r = Raytracer(width,height)
-# r.envmap = Envmap('./Assets/andromeda.bmp')
+r.envmap = Envmap('./Assets/andromeda.bmp')
 r.glClearColor(0, 0, 0)
 r.glClear()
 
 
 # Lights
-r.pointLights.append( PointLight(position = V3(-4,0,0), intensity = 0.5))
-r.pointLights.append( PointLight(position = V3( 4,0,0), intensity = 0.5))
-
-# r.dirLight = DirectionalLight(direction = V3(1, -1, -2), intensity = 0.5)
+# r.pointLights.append( PointLight(position = V3(-4,5,0), intensity = 0.5))
+r.pointLights.append( PointLight( position = V3(4   ,2.5+ 5, 0), intensity=0.5 ))
+r.pointLights.append( PointLight( position = V3(-6  ,2.5+6,-30), intensity = 0.5 , _color=color(1, 0.6, 0.0)))
+r.pointLights.append( PointLight( position = V3(1.6 ,2.5+ -2.5, -20), intensity=0.5 , _color=color(1, 0.6, 0.0) ))
 
 r.ambientLight = AmbientLight(strength = 0.1)
 
 # Objects
-# r.scene.append( Plane(V3(0,-4,10) , V3(0,1,0) , WHITE) )
-# r.scene.append( Plane(V3(0,0,10) , V3(0,0,-1) , WHITE) )
+r.scene.append( Plane(V3(0,-7,10) , V3(0,1,0) , WHITE) )
 
-# r.scene.append( Plane(V3(-10,0,-10) , V3(1,0,0) , WHITE) )
-# r.scene.append( Plane(V3(10,0,10) , V3(-1,0,0) , WHITE) )
+r.scene.append( AABB(V3(0   , -2.5 , -20) , V3(5, 0.1, 5) , glass ) )
+r.scene.append( AABB(V3(0   ,  2.5 , -20) , V3(5, 0.1, 5) , glass ) )
 
-# r.scene.append( Plane(V3(0,10,0) , V3(0,-1,0) , WHITE) )
-# r.scene.append( Plane(V3(0,-10,0) , V3(0,1,0) , WHITE) )
+r.scene.append( AABB(V3(-2.5, 0, -20) , V3(0.1, 5, 5) , glass ) )
+r.scene.append( AABB(V3(2.5 , 0, -20) , V3(0.1, 5, 5) , glass ) )
 
-# r.scene.append( Sphere(V3( 0, -2, -8), 2, sun) )
-# r.scene.append( Sphere(V3( 0, 2, -8), 2, earthMat) )
-# r.scene.append( Sphere(V3( -0.5, 0.5, -5), 0.25, stone))
-# r.scene.append( Sphere(V3( 0.25, 0.5, -5), 0.25, brick))
+r.scene.append( AABB(V3(0   , 0 ,-22.5) , V3(5,5,0.5) , glass))
+r.scene.append( AABB(V3(0   , 0 ,-17.5) , V3(5,5,0.5) , glass))
 
+r.scene.append( Sphere(V3( 0, 0, -20),2, sun))
 
-r.scene.append( AABB(V3(0   , -3  , -10 ) , V3(5, 0.1, 5) , boxMat ) )
-r.scene.append( AABB(V3(0   ,  2  , -10 ) , V3(5, 0.1, 5) , boxMat ) )
-
-r.scene.append( AABB(V3(-2.5, -0.5, -10 ) , V3(0.1, 5, 5) , boxMat ) )
-r.scene.append( AABB(V3(2.5 , -0.5, -10 ) , V3(0.1, 5, 5) , boxMat ) )
-
-r.scene.append( AABB(V3(0   ,-0.5 ,-12.5) , V3(5,5,0.5) , boxMat))
-# r.scene.append( AABB(V3(0, -3, -10), V3(5, 0.1, 5) , boxMat ) )
-# r.scene.append( AABB(V3(0, -3, -10), V3(5, 0.1, 5) , boxMat ) )
-
-# r.scene.append( AABB(V3(1.5, 1.5, -5), V3(1, 1, 1) , glass ) )
-# r.scene.append( AABB(V3(-1.5, 0, -5), V3(1, 1, 1) , boxMat ) )
-
-# r.scene.append( Sphere(V3( 0, 0, -8), 2, earthMat))
+r.scene.append( Sphere(V3( -3, -4+2.5 , -15),1, earthMat))
+r.scene.append( Sphere(V3( 0 , -5+2.5 , -10),1, orange))
+r.scene.append( Sphere(V3( 2 , 1 +2.5 , -9) , 1 , glass))
+r.scene.append( Sphere(V3( -6, -3+2.5 , -22) , 1.5 , venus))
+r.scene.append( Sphere(V3( 5 , -3+2.5 , -15) , 1 , venus))
+r.scene.append( Sphere(V3( 5 , 4 +2.5 , -15) , 1 , brick))
+r.scene.append( Sphere(V3( -4, 4 +2.5 , -15) , 1 , stone))
 
 
 # Render Scene
